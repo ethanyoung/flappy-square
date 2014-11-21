efbcb16d873a86ea3593129a2fec7346712a23ee
Ethan Yang <ethan.y@outlook.com>
2014-11-21 23:24:04 +1100

    Infrastructure

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(40, 490, Phaser.AUTO, 'game-window');
+
+var mainState = {
+	preload: function() {
+
+	},
+
+	create: function() {
+
+	},
+
+	update: function() {
+
+	},
+};
+
+game.state.add('main', mainState);
+game.state.start('main')
